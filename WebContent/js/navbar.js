a1e9d42f5748f905c059126cab21a8c8b8dff92d
vdate <vdate@ie.ibm.com>
2017-04-23 11:18:18 +0100

    navbar fixes

--- WebContent/js/navbar.js
+++ WebContent/js/navbar.js
@@ -1,17 +1,19 @@
 var navlinks = [
-	 	"<li class=\"nav\"><a href=\"index.html\">Home</a></li>",
-	 	"<li class=\"nav\"><a href=\"celebrations.html\">Celebrations</a></li>",
-	 	"<li class=\"nav\"><a href=\"weddings.html\">Weddings</a></li>",
-	 	"<li class=\"nav\"><a href=\"promotions.html\">Promotions</a></li>",
-	 	"<li class=\"nav\"><a href=\"gallery.html\">Gallery</a></li>",
-	 	"<li class=\"nav\"><a href=\"blog.html\">Blog</a></li>",
-	 	"<li class=\"nav\"><a href=\"pricing.html\">Pricing, T&C's</a></li>"
+	 	"<a href=\"index.html\">Home</a>",
+	 	"<a href=\"celebrations.html\">Celebrations</a>",
+	 	"<a href=\"weddings.html\">Weddings</a>",
+	 	"<a href=\"promotions.html\">Promotions</a>",
+	 	"<a href=\"gallery.html\">Gallery</a>",
+	 	"<a href=\"blog.html\">Blog</a>",
+	 	"<a href=\"pricing.html\">Pricing, T&C's</a>"
 ];
 
 function navbar() {
-	r = "<ul class=\"nav\">";
+	/* r = "<ul class=\"nav\">"; */
+	r ="";
 	for (i= 0; i < navlinks.length ; i++){
 		r = r +navlinks[i];
 	}
+	/* r = "</ul>" */
 	return r;
 }
